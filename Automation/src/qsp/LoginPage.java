f1e904ac3db10c39799345b2c8ac94b6fa72d72e
karthi <karthi@karthik>
2023-05-19 13:25:59 +0530

    commit

---
+++ Automation/src/qsp/LoginPage.java
@@ -0,0 +1,34 @@
+package qsp;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginPage {
+
+	static {
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+	}
+
+	public static void main(String[] args) {
+		
+		WebDriver driver=new ChromeDriver();
+		
+		driver.get("https://demo.actitime.com/login.do");
+		
+		driver.findElement(By.id("username")).sendKeys("admin");
+		driver.findElement(By.name("pwd")).sendKeys("manager");
+		
+		driver.findElement(By.xpath("//div[.='Login ']")).click();
+		
+		String title = driver.getTitle();
+		
+		System.out.println(title);
+		
+		driver.close();
+		
+		
+
+	}
+
+}
